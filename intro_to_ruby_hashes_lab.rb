263bf53dbdee7129c63b09aa4b5c2c15a2e7ce20
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-20 01:55:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,11 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   hash = {
-    :railroads => {
-      :pieces => 4,
-      :rent_in_dollars => {}
-      
-    }
+    :
   }
 end
 
@@ -16,7 +12,12 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
    hash = {
-    :railroads => {}
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {}
+      :names => {}
+      }
+    }
   }
 end
 
